83cfa72ed356622ce4c432b6363756d26d7a609f
Abdullahshq <abdullahshabbir787@gmail.com>
2025-05-14 16:24:29 +0500

    4th Commit

--- database.js
+++ database.js
@@ -1,40 +1,29 @@
+// database.js
 import sql from 'mssql';
 
+let database = null;
+
+export default class Database {
+  // (keep all methods here unchanged: connect, disconnect, create, read, etc.)
+}
+
+// ✅ Use this config in your existing class-based connection logic
 const config = {
   server: 'abddatabase.database.windows.net',
   database: 'abddb',
   authentication: {
-    type: 'azure-active-directory-msi-app' // Ensure MSI is properly enabled
+    type: 'azure-active-directory-msi-app' // for system-assigned managed identity
   },
   options: {
-    encrypt: true, // Use encryption for connection
-    trustServerCertificate: false // Set this to false if the server uses a valid certificate
+    encrypt: true,
+    trustServerCertificate: false
   }
 };
 
-export async function createDatabaseConnection() {
-  try {
-    const pool = await sql.connect(config);
-    console.log('Connected to Azure SQL');
-    
-    return {
-      readAll: async () => (await pool.request().query('SELECT * FROM Persons')).recordset,
-      read: async (id) => (await pool.request().input('id', sql.Int, id).query('SELECT * FROM Persons WHERE id = @id')).recordset[0],
-      create: async (person) => (await pool.request()
-          .input('firstName', sql.NVarChar, person.firstName)
-          .input('lastName', sql.NVarChar, person.lastName)
-          .query('INSERT INTO Persons (firstName, lastName) VALUES (@firstName, @lastName)')).rowsAffected,
-      update: async (id, person) => (await pool.request()
-          .input('id', sql.Int, id)
-          .input('firstName', sql.NVarChar, person.firstName)
-          .input('lastName', sql.NVarChar, person.lastName)
-          .query('UPDATE Persons SET firstName = @firstName, lastName = @lastName WHERE id = @id')).rowsAffected,
-      delete: async (id) => (await pool.request()
-          .input('id', sql.Int, id)
-          .query('DELETE FROM Persons WHERE id = @id')).rowsAffected
-    };
-  } catch (err) {
-    console.error('Database connection failed', err);
-    throw err;
-  }
-}
+// ✅ Do NOT switch to old function-based style
+export const createDatabaseConnection = async () => {
+  database = new Database(config);
+  await database.connect();
+  await database.createTable(); // optional
+  return database;
+};
